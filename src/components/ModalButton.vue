667e609d48f433613bbd4314e5f96510b9288cc9
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-10-25 21:06:22 +0900

    add:compositiontest

--- src/components/ModalButton.vue
+++ src/components/ModalButton.vue
@@ -16,7 +16,7 @@
 export default {
   data() {
     return {
-      modalOpen: true,
+      modalOpen: false,
     };
   },
 };
---
+++ src/views/CompositionTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>CompositionTest</div>
+</template>
+
+<script>
+export default {
+  data() {},
+  setup() {
+    console.log("setup");
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
+</script>
