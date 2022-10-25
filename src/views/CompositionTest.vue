384a1a933f3a1b32fc6523f0bd910b436f44361c
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-10-25 21:16:28 +0900

    add:setup()->ref()

--- src/views/CompositionTest.vue
+++ src/views/CompositionTest.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>CompositionTest</div>
+  <div>
+    CompositionTest
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-  data() {},
   setup() {
+    let name = "鈴木";
+    const age = 30;
+    const nameRef = ref("田中");
     console.log("setup");
+    console.log(nameRef);
+    console.log(nameRef.value);
+    return {
+      name,
+      age,
+      nameRef,
+    };
+  },
+  data() {
+    return {
+      number: 1,
+      sports: "サッカー",
+    };
   },
   created() {
     console.log("created");
+    console.log(this);
   },
   mounted() {
     console.log("mounted");
